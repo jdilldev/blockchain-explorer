bc7063cbf2fe1eda49b025ee386bc2792120cc27
Jasmine Dillard <jdilldev@gmail.com>
2025-02-17 17:31:08 -0500

    updat output of endpoint, format result as float

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
+import json
 from flask import Flask
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 import requests
+from web3 import Web3
 
 load_dotenv()
 
@@ -10,19 +12,26 @@ INFURA_API_KEY = os.getenv('INFURA_API_KEY')
 
 app = Flask(__name__)
 CORS(app)
+w3 = Web3(Web3.EthereumTesterProvider())
+
+def _convert_hex_to_decimal(hex:str):
+    wei_as_number = int(hex, 16) # convert hexstring to integer
+    return w3.from_wei(wei_as_number,'ether') # convert wei to ether
 
 @app.route("/address/balance/<address>")
-def hello_world(address):
+def get_balance(address:str):
     try:
         request_url = "https://mainnet.infura.io/v3/"+INFURA_API_KEY
-        print(request_url)
+        json_body = {"jsonrpc": "2.0", "id": 1, "method": "eth_getBalance", "params": [address, "latest"]}
         app.logger.info('INITIATED to get balance for ethereum address: %s', address)
         response = requests.post(
             url=request_url, 
-            json={"jsonrpc": "2.0", "id": 1, "method": "eth_getBalance", "params": [address, "latest"]},)
+            json=json_body
+        )
         if response.status_code == 200:
-            app.logger.info('SUCCESSFULLY to get balance for ethereum address: %s', address)
-            return response.json()['result']
+            app.logger.info('SUCCESS retrieving balance for ethereum address: %s', address)
+            balance = _convert_hex_to_decimal(response.json()['result'])
+            return json.dumps({'balance': float(balance)}) # convert Decimal to float so it can be jsonified
     except Exception as e:
         app.logger.error('FAILURE retrieving balance for ethereum address: %s', address)
         app.logger.error(e)
