a19bfb3fa03fe7d2793a2a73532d51ba60a4f832
Jasmine Dillard <jdilldev@gmail.com>
2025-02-18 01:13:31 -0500

    working images and containers

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://thedkpatel.medium.com/dockerizing-react-application-built-with-vite-a-simple-guide-4c41eb09defa
 export default defineConfig({
+	base: "/",
 	plugins: [react()],
+	preview: {
+		port: 3000,
+		strictPort: true,
+	},
 	server: {
+		port: 3000,
+		strictPort: true,
 		host: true,
+		origin: "http://0.0.0.0:3000",
 	},
 });
